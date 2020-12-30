368444d29c62193dde1ea5417e1b680976cebb95
AeuroDK <76756636+AeuroDK@users.noreply.github.com>
2020-12-30 01:22:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AddKiller_Spotify
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			try
+			{
+				using (StreamWriter streamWriter = File.AppendText("C:\\Windows\\System32\\drivers\\etc\\hosts"))
+				{
+					streamWriter.WriteLine("\r\n0.0.0.0  ads-fa.spotify.com");
+					streamWriter.WriteLine("\r\n0.0.0.0  adclick.g.doublecklick.net");
+					streamWriter.WriteLine("\r\n0.0.0.0  bounceexchange.com");
+					streamWriter.WriteLine("\r\n0.0.0.0  core.insightexpressai.com");
+					Console.WriteLine("Succesfully Blocked Spotify ads ;)");
+					Console.ReadLine();
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Something Did wrong Make Sure to Run me as An Administrator ;).");
+				Console.Read();
+			}
+		}
+	}
+}
